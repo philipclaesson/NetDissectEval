ceeb772994eab2249a03ba3aee9314452d20f395
Philip Claesson <philipcl@kth.se>
2018-12-18 22:36:43 +0000

    fixed datasplitter

--- dataset/dataSplitter.py
+++ dataset/dataSplitter.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import shutil
+import os
 
 def dataSplitter():
         import shutil
@@ -7,6 +8,9 @@ def dataSplitter():
         for q in ['q1', 'q2', 'q3', 'q4']: 
             outpath = "./broden1_224_" + q + "/images/"
             index = pd.read_csv("index_" + q + ".csv")
+            for folder in ['ade20k', 'dtd', 'opensurfaces', 'pascal']:
+                if not os.path.exists(outpath + folder):
+                    os.makedirs(outpath + folder)
             for i, row in index.iterrows():
                 shutil.copyfile(path + row['image'], outpath + row['image'])
                 shutil.copyfile(path + row["object"], outpath + row["object"])

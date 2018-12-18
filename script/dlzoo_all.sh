9ceb73818bf64bb113d37aadf1bce1842772e213
philipclaesson <philipcl@kth.se>
2018-12-18 23:34:55 +0100

    dlzoo now downloads all

--- script/dlzoo_all.sh
+++ script/dlzoo_all.sh
@@ -12,4 +12,21 @@ wget --progress=bar \
 	   http://places2.csail.mit.edu/models_places365/resnet50_places365.pth.tar
 popd
 
+echo "Download densenet161 trained on Places365"
+echo "Downloading $MODEL"
+mkdir -p zoo
+pushd zoo
+wget --progress=bar \
+	   http://places2.csail.mit.edu/models_places365/whole_densenet161_places365_python36.pth.tar
+popd
+
+
+echo "Download densenet161 trained on Places365"
+echo "Downloading $MODEL"
+mkdir -p zoo
+pushd zoo
+wget --progress=bar \
+	   http://places2.csail.mit.edu/models_places365/whole_resnet50_places365_python36.pth.tar
+popd
+
 echo "done"
